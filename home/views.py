e568d97448814a59f6c2ba91d9a0e582b56de796
Anastasiya200704 <meleskinaanastasia71@gmail.com>
2025-04-30 21:17:50 +0500

    add news page

--- home/views.py
+++ home/views.py
@@ -6,3 +6,16 @@ def credits(request):
     content = "Nicky\nAnastasiya"
     return HttpResponse (content, content_type="text/plain")
 
+def news(request):
+    data = {"news": [
+        "RiffMates now has a news page!",
+        "RiffMates has its first page"
+    ]}
+    return render(request, "news.html", data)
+
+
+
+
+
+
+
